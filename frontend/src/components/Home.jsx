c3a63a27f2a7f9ba3eacd6d7b84668dc22b559ec
Krushna <fullstack.kanha@gmail.com>
2024-10-16 23:42:11 +0530

    Refactor Home component to conditionally render CreateJob based on user role

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import CreateJob from "./CreateJob";
 import AllJobs from "./AllJobs";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+
   return (
     <div>
-      <CreateJob />
+      {authUser && authUser.role === "company" && <CreateJob />}
+
       <AllJobs />
     </div>
   );
